4a90d31ed64e246c60538b45a6be7972cf0f6a1e
Rio-grey <lequy11082003@gmail.com>
2023-02-09 11:22:44 +0700

    add link item project

--- pages/projects.js
+++ pages/projects.js
@@ -1,11 +1,11 @@
-import { projects } from "../lib";
+import { projects } from "../data";
 import Header from "../components/Header";
 const ProjectsPage = () => {
   return /*html*/ `
     ${Header()}
     ${projects
       .map((project) => {
-        return `<div class="project-item">${project.name}</div>`;
+        return `<div class="project-item"><a href="/project/${project.id}">${project.name}</a></div>`;
       })
       .join("")}
   `;
