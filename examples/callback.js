3cc715cef3289e846ed46ccc033d2cf2f8c87921
Rio-grey <lequy11082003@gmail.com>
2023-02-09 12:38:35 +0700

    xử lý bất đồng bộ

--- examples/callback.js
+++ examples/callback.js
@@ -7,3 +7,23 @@ const myCalculator = (a, b, callback) => {
 myCalculator(5, 5, myDisplay);
 
 // truyền 1 hàm vào 1 hàm khác như một tham số
+
+// Xử lý bất đồng bộ
+function loadScript(src, callback) {
+  const script = document.createElement("script"); // tạo ra thẻ <script>
+  script.src = src; // <script src="https://github.com/"></script>
+  script.onload = function () {
+    callback(script);
+  };
+  script.onerror = function () {
+    callback(new Error("load failed"));
+  };
+  document.head.append(script); // <head><script src="https://github.com/"></script></head>
+}
+loadScript("https://github.com/", function (script) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("script loaded: ", script);
+  }
+});
